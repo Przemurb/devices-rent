ba9e1459eae3ad30d07a544faaea36541321bf4f
Przemurb <przemurb@hotmail.it>
2022-04-20 17:30:15 +0200

    Initial commit

---
+++ src/test/java/com/example/devicesrent/DevicesRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devicesrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
